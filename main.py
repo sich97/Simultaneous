ff58951a10518ac710e9b193ba218521df99abfa
Simon Christiansen <sich97@gmail.com>
2021-10-22 19:48:40 +0200

    Initial working code

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pyautogui
+import time
+
+
 def main():
-    pass
+    pyautogui.PAUSE = 0.0
+
+    time.sleep(5)
+    first_spot = pyautogui.position()
+
+    time.sleep(5)
+    second_spot = pyautogui.position()
+
+    time.sleep(5)
+    pyautogui.leftClick(first_spot)
+    pyautogui.leftClick(second_spot)
 
 
 if __name__ == "__main__":
